986d2ea2fee7c1e61b205595623e212e5ca0deb9
foisal404 <foisalahmmed2@gmail.com>
2023-11-16 15:21:38 +0600

    Change COntent in Navbar

--- src/components/Navhead.jsx
+++ src/components/Navhead.jsx
@@ -9,32 +9,33 @@ const Navhead = () => {
         <Navbar.Container className="flex items-center justify-between py-2">
           <Navbar.Container className="flex items-center">
             <Navbar.Brand>
-              <a className="uppercase futura-font text-2xl text-purple-600 cursor-pointer">Course Monster</a>
+              <a className="uppercase futura-font sm:text-lg md:text-2xl text-purple-600 cursor-pointer">Course Monster</a>
             </Navbar.Brand>
             <Navbar.Divider></Navbar.Divider>
-            {/* <Navbar.Container
+            <Navbar.Container
               tag="ul"
-              className="lg:flex hidden items-center justify-between gap-8"
+              className="lg:flex hidden items-center justify-between gap-8 text-sm"
             >
-              <Navbar.Link linkName="Home" />
-              <Navbar.Link linkName="Projects" />
-              <Navbar.Link linkName="About" />
+              <Navbar.Link linkName="Home"  className='uppercase text-purple-100'/>
+              <Navbar.Link linkName="About Us" className='uppercase text-purple-100'/>
+              {/* <Navbar.Link linkName="Projects" /> */}
             </Navbar.Container>
             <Navbar.Collapse collapseType="sidebar">
               <Navbar.Container tag="ul" className="flex flex-col gap-5">
-                <Navbar.Link linkName="Home" />
-                <Navbar.Link linkName="Projects" />
+              <Navbar.Link linkName="Home"  className='uppercase text-purple-100'/>
+              <Navbar.Link linkName="About Us" className='uppercase text-purple-100'/>
+                {/* <Navbar.Link linkName="Projects" />
                 <Navbar.Link linkName="Blogs" />
                 <Navbar.Link linkName="News" />
-                <Navbar.Link linkName="Resources" />
+                <Navbar.Link linkName="Resources" /> */}
               </Navbar.Container>
-            </Navbar.Collapse> */}
+            </Navbar.Collapse>
           </Navbar.Container>
 
           <Navbar.Container className="flex gap-2 tex">
             
-            <Button size="sm"  customClass='px-10 uppercase'  >All Courses</Button>
-            <Button size="sm" color customClass='px-10 bg-[#9C35DE] text-[#fff] uppercase'>
+            <Button size="sm"  customClass='md:px-10 uppercase '  >All Courses</Button>
+            <Button size="sm" color customClass='md:px-10 bg-[#9C35DE] text-[#fff] uppercase'>
               Login 
             </Button>
             <Navbar.Toggle />
